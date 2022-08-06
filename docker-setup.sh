b02cf7803eba6bdea8fc94e9141c832752931094
dataopskenn <108646604+dataopskenn@users.noreply.github.com>
2022-08-06 18:16:50 +0100

    updated docker-setup.sh
    added the 3rd, 4th and 5th lines

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,2 +1,5 @@
-snap install docker 
-docker run hello-world
+sudo snap install docker 
+sudo docker run hello-world
+sudo docker build -t test:pycontainer
+sudo docker run -it test:pycontainer
+python pipeline.py
